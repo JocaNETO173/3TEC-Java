498c189d98e2f7b5fd457ad5e5d17b263fe04024
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-07-02 08:52:09 -0300

    Add files via upload

---
+++ Aula11_MetodoParametros.java
@@ -0,0 +1,6 @@
+public class Aula11_MetodoParametros {
+    public static void main(String[] args){
+        CalculadoraSimples calc = new CalculadoraSimples();
+        calc.somarDoisNumeros(5, 30);
+    }
+}
