5598c71a06d1fdc6376dbaf1028b15ae99d604e0
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-07-02 08:47:46 -0300

    Add files via upload

---
+++ CalculadoraSimples.java
@@ -0,0 +1,6 @@
+public class CalculadoraSimples {
+    void somarDoisNumeros(int numA, int numB){
+        int resultado = numA + numB;
+        System.out.println("Resultado da soma: "+ resultado);
+    }
+}
